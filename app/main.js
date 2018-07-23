6ae4999f9b9fa5d9b95e10571a5f06178e7be440
yoopig <214079278@qq.com>
2018-07-23 11:57:36 +0800

    基础webpack 配置

---
+++ app/main.js
@@ -0,0 +1,3 @@
+import {firstName, lastName, year} from './greeter';
+console.log(year);
+document.querySelector("#root").innerHTML = firstName + lastName 
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,33 @@
-const path = require('path');
+// 一个常见的`webpack`配置文件
+const webpack = require('webpack');
 
-const config  = { 
-    entry:"./index.js",
-    output:{
-        path:path.resolve(__dirname,'dist'),
-        filename:'bundle.js'
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-        port: 9000,
-        historyApiFallback:true,
-        inline:true,
-        hot:true,
-      }
+
+module.exports = {
+        entry: __dirname + "/app/main.js", //已多次提及的唯一入口文件
+        output: {
+            path: __dirname + "/dist",//打包后的文件存放的地方
+            filename: "bundle.js"//打包后输出文件的文件名
+        },
+        devtool: 'none',
+        devServer: {
+            contentBase: "./", //本地服务器所加载的页面所在的目录
+            historyApiFallback: true, //不跳转
+            inline: true,
+            hot: true
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+          ],   
+        module: {  
+            // es6 转 es5
+            rules: [{  
+                test:/\.(js|jsx)$/,  
+                exclude: /node_modules/,  
+                loader: 'babel-loader'  
+            }]  
+        }  ,
+        devtool: 'eval-source-map',  //生成Source Maps
+        mode:'development'
+
+
 }
-
-module.exports =  config;
