eebc16a22bdc2328d4791763ac6cd8a09f4053fe
yoopig <214079278@qq.com>
2018-07-25 18:24:31 +0800

    奠定框架基础

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 import Vision from '../lib/vision/vision'
 
    new Vision(
-     <div  class="hello">
+     <div  class="hello" Click={()=>{ console.log(321312)}}>
             <section>
                 1111111111111111111111111111111
                 <div>321321321</div>
