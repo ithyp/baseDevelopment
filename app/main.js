290b49fa2ad49013adcecfd266e3bac11f070df9
yoopig <214079278@qq.com>
2018-07-26 15:39:43 +0800

    框架0.0.1版本 无类组件

--- app/main.js
+++ app/main.js
@@ -1,9 +1,62 @@
 import Vision from '../lib/vision/vision'
+ 
+ let Ele = <div>你好，世界!</div>;
+
+
+
+ function EleFn(props){
+    return (<ul>
+                <li>{props.a}</li>
+                <li>小红</li>
+                <li>小绿</li>
+                <li>小蓝</li>
+          </ul>)
+  }
+
+class EleClass{
+
+    view(){
+        return <table border="1">
+                    <tr>
+                        <th>Month</th>
+                        <th>Savings</th>
+                    </tr>
+                    <tr>
+                        <td>January</td>
+                        <td>$100</td>
+                    </tr>
+                </table>
+    }
+    options(){
+        return {
+            props:{
+
+            },
+            data:{
+                
+            },
+            methods:{
+
+            },
+            computed:{
+
+            },
+            watch:{
+
+            }
+        }
+    } 
+}
+
 
    new Vision(
-     <div  class="hello" Click={()=>{ console.log(321312)}}>
-            <section>
-                1111111111111111111111111111111
-                <div>321321321</div>
-            </section>
-     </div>,document.getElementById("app")); 
+
+     <section  Click={()=>{ console.log("事件点击")}}>
+           {/* <h1>Hello,World!</h1>
+           <Ele></Ele>
+           <EleFn a="小白"></EleFn> */}
+           <EleClass></EleClass>
+     </section>
+
+     , document.getElementById("app")
+    ); 
